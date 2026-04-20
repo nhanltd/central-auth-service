be0101de4a0a3b4bed9b97f2fd854c49bc7e01d0
kelvinthebeast <thanhnhan13082021@gmail.com>
2026-04-19 22:58:41 -0700

    base project

---
+++ src/main/java/com/nhanthanhle/centralauthservice/CentralAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nhanthanhle.centralauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralAuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CentralAuthServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nhanthanhle/centralauthservice/CentralAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhanthanhle.centralauthservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralAuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
